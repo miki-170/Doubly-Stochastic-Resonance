a73356c30e8b70139122d44dd776606be64d1386
miki-170 <lesiakmikolaj@gmail.com>
2025-10-12 20:41:09 +0100

    Tried implementing the varying values but still not fixed

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import numpy as np
 
 # Initial Conditions on the system - close to the solution (mean 0)
 
-def initial_cond(N,c):
+def initial_cond(N,c,v=1):
     x_n=np.zeros((N,N),dtype=float)
     for i in range(N):
         for j in range(N):
-            x_n[i,j]=c*np.random.normal(0)
+            x_n[i,j]=c*np.random.normal(0,np.sqrt(v))
     return x_n
 
 # Function f from the defintion
@@ -59,13 +59,13 @@ D=20
 d=2
 
 # Total number of steps
-Lim=10000
+Lim=1000
 
 # Initialise time 
 t=0
 
 # Time step
-dt=1/Lim
+dt=0.001
 
 # Coefficient for generating IC
 c = 0.0001
@@ -80,63 +80,84 @@ tmp=Lim/N_p
 #__________________________
 # Main 
 
-# Create the system
-x=np.zeros((N,N),dtype=float)
+
 
 
 #First Order Scheme
 #_________________________
 
-#Space for m_values
-m_values=[]
-
-for k in range(Lim):
-
-    # Main step
-    x_n=initial_cond(N,c)
-
-    # Create noises
-
-    dzeta=initial_cond(N,1)
-    xi=initial_cond(N,1)
+#Space for values for simulation
 
 
-    # sq_m and sq_a
+dzeta_var_values=[3]
 
-    sq_m=np.sqrt(xi.var()*dt)
+xi_var_values=[2]
 
-    sq_a=np.sqrt(dzeta.var()*dt)
+m_final=[]
 
-    # Boundary condtitions
+for l in range(1):
+    dzes=3
+    for xis in xi_var_values:
 
+        # Create the system
+        x=np.zeros((N,N),dtype=float)
 
-    for i in range(N):
-        x_n[N-1][i]=x[1][i]
-        x_n[0][i]=x[N-2][i]
-        x_n[i][N-1]=x[i][1]
-        x_n[i][0]=x[i][N-2]
+        m_values=[]
+        # Main step
+        x_n=initial_cond(N,c)
 
+        for k in range(Lim):
 
+            
 
-    x_n=integration_step(N,x,x_n,D,d,dt,xi,dzeta,sq_m,sq_a)
+            # Create noises
 
-
-    t,x = update_system(t,dt,x_n)
-
-    # Printing N_p results while modelling
-    """if (k+1)/tmp==int((k+1)/tmp):
-        print("\n")
-        print(f"Step {k+1}")
-        print("\n")
-        print(x) 
-        print("\n")"""
-    # Calculating average state of the system
-    m_instant = av_state(x)
-
-    m_values.append(m_instant)
-
-m_average=np.mean(np.abs(m_values))
-print(m_average)
+            dzeta=initial_cond(N,1,dzes)
+            xi=initial_cond(N,1,xis)
 
 
-    
+            # sq_m and sq_a
+
+            sq_m=np.sqrt(xi.var()*dt)
+
+            sq_a=np.sqrt(dzeta.var()*dt)
+
+            # Boundary condtitions
+
+
+            for i in range(N):
+                x_n[N-1][i]=x[1][i]
+                x_n[0][i]=x[N-2][i]
+                x_n[i][N-1]=x[i][1]
+                x_n[i][0]=x[i][N-2]
+
+
+
+            x_n=integration_step(N,x,x_n,D,d,dt,xi,dzeta,sq_m,sq_a)
+
+
+            t,x = update_system(t,dt,x_n)
+
+            # Printing N_p results while modelling
+            if (k+1)/tmp==int((k+1)/tmp):
+                print("\n")
+                print(f"Step {k+1}")
+                print("\n")
+                print(x) 
+                print("\n")
+            # Calculating average state of the system
+            m_instant = av_state(x)
+
+            m_values.append(m_instant)
+
+
+        del(x_n)
+        m_average=np.mean(np.abs(m_values))
+
+        m_final.append((xis,m_average))
+
+print(m_final)
+
+
+
+        
