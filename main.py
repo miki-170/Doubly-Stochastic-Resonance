656031291c0d61c2a4c6e75d92a3b84884fcb521
miki-170 <lesiakmikolaj@gmail.com>
2025-10-14 22:19:55 +0100

    Finalised issues with divergence, and the model for evaluation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Importing neccesary libraries
 import numpy as np 
+import matplotlib.pyplot as plt
 
 
 
@@ -8,16 +9,14 @@ import numpy as np
 
 # Initial Conditions on the system - close to the solution (mean 0)
 
-def initial_cond(N,c):
-    x_n=np.zeros((N,N),dtype=float)
-    for i in range(N):
-        for j in range(N):
-            x_n[i,j]=c*np.random.normal(0)
-    return x_n
+def initial_cond(N,v=2):
+    return np.random.normal(0,v,(N,N))
+   
 
 # Function f from the defintion
 def f(x):
-    return -x * (1+x**2)**2
+    
+    return (-x) * (1+(x + 1e-8)**2)**2
 
 # Function g from the definition 
 def g(x):
@@ -27,30 +26,37 @@ def g(x):
 def der_g(x):
     return 2*x
 
-# Integration step for first order scheme
-def integration_step(N,x,x_n,D,d,dt,xi,dzeta,sq_m,sq_a):
-    xi_var=xi.var()
-    
+# Intermediate integrating step for second order scheme
+def inter_integration_step(N,x,x_tmr,D,d,dt,xi,dzeta,sq_m,sq_a):
+
     for i in range(1,N-1):
         for j in range(1,N-1):
-            x_n[i,j] = x[i][j] + dt * (f(x[i][j]) + D/d * (x[i+1][j] + x[i-1][j]+ x[i][j+1] + x[i][j-1] - d * x[i][j]) + xi_var/2 * g(x[i][j]) * der_g(x[i][j])) + sq_m * g(x[i][j]) * xi[i][j] + sq_a * dzeta[i][j]
-    
+            x_tmr[i,j] = x[i][j] + dt * (f(x[i][j]) + D/(d) * (x[i+1][j] + x[i-1][j]+ x[i][j+1] + x[i][j-1] - d * x[i][j]))  + sq_m * g(x[i][j]) * xi[i][j] + sq_a * dzeta[i][j]
+   
+    return x_tmr
+
+# Main integrating step for second order scheme
+
+def main_integration_step(N,x,x_n,x_tmr,D,d,dt,sq_m,sq_a,xi,dzeta):
+
+    for i in range(1,N-1):
+        for j in range(1,N-1):
+            x_n[i][j] = x[i][j]+( f(x[i][j])+ D/d * (x[i+1][j] + x[i-1][j]+ x[i][j+1] + x[i][j-1] - d* x[i][j]) +f(x_tmr[i][j] + D/d * (x_tmr[i+1][j] + x_tmr[i-1][j]+ x_tmr[i][j+1] + x_tmr[i][j-1] - d* x_tmr[i][j])) )*dt/2 + sq_m * g(x[i][j]) * xi[i][j]/2 + sq_m * g(x_tmr[i][j])*xi[i][j]/2 + sq_a * dzeta[i][j]
+
     return x_n
 
 # Updating system with new t and x after integration
 def update_system(t,dt,x_n):
     return (t+dt,x_n)
 
-# Average of the function: 
-def av_state(x):
-    return np.mean(x)
+
 
 
 #_______________________
 # Constants
 
 # Size of the grid
-N=4
+N=10
 
 # Strengh of the coupling
 D=20
@@ -58,85 +64,141 @@ D=20
 # Dimensions
 d=2
 
-# Total number of steps
-Lim=10000
-
 # Initialise time 
 t=0
 
+# Total time
+
+T=10
+
 # Time step
-dt=1/Lim
+dt=0.00001
+
+# Total number of steps
+Lim=int(1/dt)
+
+# Number of simulations 
+
+S=1
+
 
 # Coefficient for generating IC
 c = 0.0001
 
-# The number of printable steps
-N_p=10
-
 # Auxilary constant for printing
-tmp=Lim/N_p
+tmp=Lim/T
 
+# print solutions A=1 yes A=0 no
+A=0
 
 #__________________________
 # Main 
 
-# Create the system
-x=np.zeros((N,N),dtype=float)
-
-
-#First Order Scheme
+#Second Order Scheme
 #_________________________
 
-#Space for m_values
-m_values=[]
-
-for k in range(Lim):
-
-    # Main step
-    x_n=initial_cond(N,c)
-
-    # Create noises
-
-    dzeta=initial_cond(N,1)
-    xi=initial_cond(N,1)
+#Space for values for simulation
 
 
-    # sq_m and sq_a
+dzeta_var_values=[3]
 
-    sq_m=np.sqrt(xi.var()*dt)
+xi_var_values=[5]
 
-    sq_a=np.sqrt(dzeta.var()*dt)
+m_final=[]
 
-    # Boundary condtitions
-
-
-    for i in range(N):
-        x_n[N-1][i]=x[1][i]
-        x_n[0][i]=x[N-2][i]
-        x_n[i][N-1]=x[i][1]
-        x_n[i][0]=x[i][N-2]
+for repetition in range(S):
+    m_values=[]
 
 
 
-    x_n=integration_step(N,x,x_n,D,d,dt,xi,dzeta,sq_m,sq_a)
+    for k in range(Lim):
+        
+        # Create new system
+        if k==0:
+            x=np.zeros((N,N))
+            x_n=initial_cond(N)*c
+        
+
+        # Calculating average state of the system
+        
+        m_values.append(np.mean(x))
+
+        # Create intermediate step
+
+        x_tmr=np.zeros((N,N))
+
+        # Create noises
+
+        dzeta=initial_cond(N,1)*np.sqrt(dt)
+        xi=initial_cond(N,1)*np.sqrt(dt)
+
+        # sq_m and sq_a
+
+        sq_m=np.sqrt(xi.var()*dt)
+
+        sq_a=np.sqrt(dzeta.var()*dt)
+
+        # Boundary condtitions
+
+        x_tmr[N-1][:]=x[1][:]
+        x_tmr[0][:]=x[N-2][:]
+        x_tmr[:][N-1]=x[:][1]
+        x_tmr[:][0]=x[:][N-2]
+
+        # Predictor step
+        
+        x_tmr=inter_integration_step(N,x,x_tmr,D,d,dt,xi,dzeta,sq_m,sq_a)
+
+        
+        # Boundary conditions for the main step
+
+        x_n[N-1][:]=x_tmr[1][:]
+        x_n[0][:]=x_tmr[N-2][:]
+        x_n[:][N-1]=x_tmr[:][1]
+        x_n[:][0]=x_tmr[:][N-2]
 
 
-    t,x = update_system(t,dt,x_n)
+        # Main Step
 
-    # Printing N_p results while modelling
-    """if (k+1)/tmp==int((k+1)/tmp):
-        print("\n")
-        print(f"Step {k+1}")
-        print("\n")
-        print(x) 
-        print("\n")"""
-    # Calculating average state of the system
-    m_instant = av_state(x)
-
-    m_values.append(m_instant)
-
-m_average=np.mean(np.abs(m_values))
-print(m_average)
+        x_n = main_integration_step(N,x,x_n,x_tmr,D,d,dt,sq_m,sq_a,xi,dzeta)
 
 
-    
+        # Updating the system 
+        t,x = update_system(t, dt, x_n)
+
+        
+
+        if A==1:
+            # Printing N_p results while modelling
+            if (k+1)/tmp==int((k+1)/tmp):
+                print("\n")
+                print(f"Step {k+1}")
+                print("\n")
+                print(x) 
+                print("\n")
+        
+        
+        del(x_tmr)
+
+    m_values.append(np.mean(x))
+    xs=np.linspace(0,T,len(m_values))
+
+
+    plt.plot(xs,m_values)
+
+
+# Adjusting the graph
+plt.grid()
+plt.xlim(0,10)
+plt.ylabel("Average field of oscilators")
+plt.xlabel("Time")
+
+
+plt.show()
+
+
+
+
+
+
+        
